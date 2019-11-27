02a3a5cfa6903f545cbc2eb10ff23b386a53a1d5
Tiago Póvoa <tiago.quin@gmail.com>
2019-11-27 17:06:20 +0100

    add mutex, shall test it

--- labo2/main.go
+++ labo2/main.go
@@ -1,17 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 import "prr-labo2/labo2/mutex"
+
+type NetworkMock struct {}
+
+func (n NetworkMock) Req(stamp uint32, id uint16){}
+func (n NetworkMock) Ok(stamp uint32, id uint16){}
+func (n NetworkMock) Update(value uint32){}
 
 func main() {
 	fmt.Println("Hello words")
 
 	mutex := mutex.Mutex{}
-
-	mutex.Init()
+ 	n := NetworkMock{}
+	mutex.Init(1, 1, n)
 
 	mutex.Ask()
 
-	mutex.Wait()
+	mutex.Req(3,3)
+
+ 	time.Sleep(time.Second * 1)
 
 }
