a68c40d13ae75180324e41773827c3dd2ab5af15
Burgener francois <francois.burgener@hotmail.fr>
2019-12-02 13:36:47 +0100

    test script shell for linux

---
+++ labo2/startLinux.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+echo "Bash version ${BASH_VERSION}..."
+for i in {0..10..1}
+  do 
+     echo "Welcome $i times"
+ done
+#gnome-terminal -x bash -c "<my command or script>; exec bash"
